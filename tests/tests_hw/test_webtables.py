08bf125a966570b951e914932e5e2cd4e5cd52ba
Dmitriy <v.dmitriy_v@mail.ru>
2025-10-13 21:28:27 +0300

    ### ДЗ №10 1. **test_text_box.py** - Тесты формы Text Box 2. **test_login_form_validate.py** - Тесты валидации формы 3. **test_login_form.py** - Тесты для заполнения State and City
    ### ДЗ №11
    4. **test_webtables.py** - Тесты для Web Tables
    
    ## Требования
    
    - Python 3.7+
    - Chrome браузер
    - ChromeDriver
    
    ## Установка зависимостей
    
    ```bash
    pip install selenium pytest

--- tests/tests_hw/test_webtables.py
+++ tests/tests_hw/test_webtables.py
@@ -1,36 +1,21 @@
-# import time
-# import allure
-# from page_tables import Tables
-#
-#
-# def test_tables(browser):
-#     page_tables = Tables(browser)
-#
-#     page_tables.visit()
-#     assert not page_tables.no_data.exist()
-#
-#
-#     while page_tables.btn_delete_row.exist():
-#         page_tables.btn_delete_row.click()
-#
-#         time.sleep(2)
-#         assert page_tables.no_data.exist()
-
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support.ui import Select
 
 
 def test_webtables_add_edit_delete():
     driver = webdriver.Chrome()
     driver.get("https://demoqa.com/webtables")
+    time.sleep(2)
 
     driver.find_element(By.ID, "addNewRecordButton").click()
     time.sleep(1)
+    assert driver.find_element(By.ID, "firstName").is_displayed()
 
     driver.find_element(By.ID, "submit").click()
     time.sleep(1)
+    assert driver.find_element(By.ID, "firstName").is_displayed()
 
     driver.find_element(By.ID, "firstName").send_keys("Ivan")
     driver.find_element(By.ID, "lastName").send_keys("Ivanov")
@@ -41,9 +26,12 @@ def test_webtables_add_edit_delete():
 
     driver.find_element(By.ID, "submit").click()
     time.sleep(2)
+    assert "Ivan" in driver.page_source
+    assert "Ivanov" in driver.page_source
 
     driver.find_element(By.ID, "edit-record-4").click()
     time.sleep(1)
+    assert driver.find_element(By.ID, "firstName").is_displayed()
 
     first_name = driver.find_element(By.ID, "firstName")
     first_name.clear()
@@ -51,9 +39,11 @@ def test_webtables_add_edit_delete():
 
     driver.find_element(By.ID, "submit").click()
     time.sleep(2)
+    assert "Petr" in driver.page_source
 
     driver.find_element(By.ID, "delete-record-4").click()
     time.sleep(2)
+    assert "Petr" not in driver.page_source
 
     driver.quit()
 
@@ -61,18 +51,17 @@ def test_webtables_add_edit_delete():
 def test_webtables_pagination():
     driver = webdriver.Chrome()
     driver.get("https://demoqa.com/webtables")
+    time.sleep(2)
 
-    rows_select = driver.find_element(By.XPATH, "//select")
-    driver.execute_script("arguments[0].scrollIntoView();", rows_select)
-    time.sleep(1)
-    rows_select.click()
-    time.sleep(1)
-
-    driver.find_element(By.XPATH, "//option[@value='5']").click()
+    select = Select(driver.find_element(By.XPATH, "//select"))
+    select.select_by_value("5")
     time.sleep(2)
 
     next_button = driver.find_element(By.XPATH, "//button[text()='Next']")
     previous_button = driver.find_element(By.XPATH, "//button[text()='Previous']")
+
+    assert next_button.get_attribute("disabled") is not None
+    assert previous_button.get_attribute("disabled") is not None
 
     for i in range(3):
         driver.find_element(By.ID, "addNewRecordButton").click()
@@ -87,6 +76,11 @@ def test_webtables_pagination():
 
         driver.find_element(By.ID, "submit").click()
         time.sleep(2)
+
+    page_info = driver.find_element(By.XPATH, "//span[@class='-pageInfo']").text
+    assert "2" in page_info
+
+    assert next_button.get_attribute("disabled") is None
 
     next_button.click()
     time.sleep(2)
